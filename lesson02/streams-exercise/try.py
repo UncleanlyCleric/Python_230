96667c241103abf4c3acc6d583dd8a750b502fc9
UncleanlyClericr <junyatwin@gmail.com>
2019-06-24 14:49:57 -0700

    Okay, this one is done

---
+++ lesson02/streams-exercise/try.py
@@ -0,0 +1,15 @@
+"""
+USAGE: `python try.py 123456`
+"""
+# pylint: disable = C0103
+
+import io
+import sys
+
+from stream_exercise import StreamProcessor
+
+value = sys.argv[1]
+
+my_stream_processor = StreamProcessor(io.StringIO(value))
+result = my_stream_processor.process()
+print("Processed {} and got {}".format(value, result))
