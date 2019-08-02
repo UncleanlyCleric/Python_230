499433d7e1753f34bdfb00950070c33e4467a861
UncleanlyClericr <junyatwin@gmail.com>
2019-08-02 08:45:13 -0700

    Fixing again

--- lesson06/django_blog/mysite/urls.py
+++ lesson06/django_blog/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('polling/', include('polling.urls')),
+    path('polling/', include('polling.urls')),  # <-- Add this
     path('admin/', admin.site.urls),
 ]
